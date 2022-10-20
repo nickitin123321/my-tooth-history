84aa2869aad192b0b8a6b6187e60427e02462755
Nikita Nikitin <nickitin_2010@mail.ru>
2022-10-20 14:27:11 +0300

    fix layout + add tooltip controls

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,10 +1,23 @@
 import './Tooltip.css';
+import TooltipControls from './TooltipControls';
+import { useState,  MouseEventHandler, MouseEvent } from 'react';
 
 const Tooltip = ({ top, left }: { top: string; left: string }) => {
+  const [isInputVisible, setIsInputVisible] = useState(false);
+  const items = [<li className='content-item'>HuiHui</li>, <li className='content-item'>HuiHui</li>];
+
+  const handleOnClick: MouseEventHandler = (e: MouseEvent) =>{
+    setIsInputVisible((prev)=> !prev);
+  }
+
   return (
     <div className="tooltip" style={{ top: top, left: left }}>
-      <div className="cross">&#10006;</div>
-      <div className="content">Hui</div>
+      <button className="cross">&#10006;</button>
+      <ul className="content">
+       {items}
+       {isInputVisible && <TooltipControls/>}
+      </ul>
+      <button className='plus' onClick={handleOnClick}>⊕</button>
     </div>
   );
 };
---
+++ src/components/Tooltip/TooltipControls/TooltipControls.tsx
@@ -0,0 +1,11 @@
+import './TooltipControls.css'
+
+const TooltipControls = () => {
+  return(
+  <div>
+    <input className='input'></input>
+    <button className='ok-button'>ok</button>
+  </div>
+  )
+}
+export default TooltipControls
