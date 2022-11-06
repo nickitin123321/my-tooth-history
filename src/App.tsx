87c0a13f48329210fd153560986ec9f87703c7eb
Nikita Nikitin <nickitin_2010@mail.ru>
2022-11-06 14:41:54 +0300

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,5 @@
-import React, { useState } from 'react';
-import { MouseEventHandler, MouseEvent } from 'react';
-import Container from './components/Container';
-import Tooltip from './components/Tooltip';
-import Head from './components/Head';
+import Router from './Router';
 
-type Coordinates = {
-  top: string;
-  left: string;
-};
-export type SetCoordinates = React.Dispatch<React.SetStateAction<Coordinates>>;
-
-const INITIAL_STATE: Coordinates = {
-  top: '120px',
-  left: '-120%',
-};
-
-function App() {
-  const [{ top, left }, setCoordinates] = useState(INITIAL_STATE);
-
-  const handleOnClick: MouseEventHandler<HTMLElement> = (
-    e: MouseEvent<Element | SVGAElement>
-  ) => {
-    const target = e.target as Element | SVGAElement;
-    const {className: { baseVal }, className} = target;
-    console.log(target);
-    if (baseVal === 'tooth') {
-      setCoordinates({
-        top: `${120}px`,
-        left: `${10}%`,
-      });
-    } else if (baseVal === 'close') {
-      setCoordinates(INITIAL_STATE);
-    }
-  };
-
-  return (
-    <div onClick={handleOnClick}>
-      <Head />
-      {<div className={left === '-120%'? 'visible' : 'hidden'}>Write</div>}
-      <Tooltip top={top} left={left} />
-      <Container />
-    </div>
-  );
+export default function App(): JSX.Element {
+  return <Router />;
 }
-
-export default App;
--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,6 +1,32 @@
-import './Router.css'
+//import React, { useContext } from 'react';
+import { Route, Routes, Navigate, BrowserRouter } from 'react-router-dom';
 
-const Router = () => {
-  return <div className='router'></div>
+import SignIn  from '../pages/SingIn';
+import SignUp from '../pages/SingUp';
+import Home from '../pages/Home';
+
+//import { SignInContext } from '../contexts';
+
+
+export default function Router(): JSX.Element {
+  //const { isAuthenticated } = useContext(SignInContext);
+  if (false) {
+    return (
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+      </BrowserRouter>
+    );
+  }
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="signin" element={<SignIn/>} />
+      <Route path="signup" element={<SignUp/>} />
+      <Navigate to="signin" />
+    </Routes>
+    </BrowserRouter>
+  );
 }
-export default Router
